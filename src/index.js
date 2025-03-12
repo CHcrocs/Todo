9914237433e68dca02ff2577977fdc26adbd24a2
CHcrocs <138597970+CHcrocs@users.noreply.github.com>
2025-03-12 14:50:45 -0300

    restarting

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 class Todo {
-  constructor(title, description, dueDate, priority, notes, checklist) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-    this.checklist = checklist;
+  constructor(title, description, dueDate, priority, notes = "", checklist = []) {
+    this.title = title,           // Title of the task
+    this.description = description, // Description of the task
+    this.dueDate = dueDate,       // Due date for completion
+    this.priority = priority,     // Priority level (High, Medium, Low)
+    this.notes = notes,           // Optional notes about the task
+    this.checklist = checklist,   // Optional checklist (array of sub-tasks)
+    this.completed = false       // Status if the task is completed or not
+  }
+
+  changeCompleted(){
+    this.completed = !this.completed
   }
 }
 
-class Project {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-    this.todos = [];
+class Projects {
+  constructor (name){
+    this.name = name,
+    this.todos = []
   }
 
-  addTodo(todo) {
-    this.todos.push(todo);
+  addTodo(todo){
+    this.todos.push(todo)
   }
 
-  removeTodo(todoId) {
-    this.todos = this.todos.filter((todo) => todo.id !== todoId);
+  removeTodo(title){
+    this.todos = this.todos.filter(todo => todo.title !== title)
   }
 }
+
