005fa947253e21cea3cd1b90aa24de6224118c72
Crocs <138597970+CHcrocs@users.noreply.github.com>
2025-03-31 13:21:33 -0300

    render todo

--- src/dom.js
+++ src/dom.js
@@ -27,10 +27,42 @@ export function renderProjects() {
 
         projectItem.addEventListener("click", () => {
             currentProject = project;
-            // renderTodos
+            renderTodos();
             renderProjects();
         });
         
         projectContainer.appendChild(projectItem);
     });
+}
+
+export function renderTodos() {
+    todoListContainer.innerHTML = "";
+
+    if (!currentProject) 
+        return;
+
+    currentProject.todos.forEach(todo => {
+        const todoItem = document.createElement("div");
+        todoItem.classList.add("todo_item");
+        todoItem.style.borderLeft = `5px solid ${getPriorityColor(todo.priority)}`;
+
+        todoItem.innerHTML = `
+            <h3>${todo.title}</h3>
+            <p><strong>Data:</strong> ${todo.dueDate}</p>
+            <button class="expand_btn" data_id="${todo.id}">✏️</button>
+            <button class="delete_btn" data_id="${todo.id}">🗑️</button>
+        `;
+
+        todoItem.querySelector(".expand_btn").addEventListener("click", () => {
+            // Editar todo
+        });
+
+        todoItem.querySelector(".delete_btn").addEventListener("click", () => {
+            currentProject.removeTodo(todo.id);
+            renderTodos();
+        });
+
+        todoListContainer.appendChild(todoItem);
+    })
+
 }
